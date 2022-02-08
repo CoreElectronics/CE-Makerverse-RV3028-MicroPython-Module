2bcf94ccaa23fd3e95fdee5c42e2727e66d8b4f8
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2022-02-08 17:22:25 +1100

    Update and rename rv3028_example.py to Makerverse_RV3028_example.py

--- Makerverse_RV3028_example.py
+++ Makerverse_RV3028_example.py
@@ -1,8 +1,8 @@
 from machine import I2C, Pin
-import rv3028
+from Makerverse_RV3028 import Makerverse_RV3028
 
 #i2c = I2C(0, sda = Pin(0), scl = Pin(1))
 i2c = I2C(0, sda=Pin(8), scl=Pin(9))
-rtc = rv3028.rv3028(i2c = i2c)
+rtc = Makerverse_RV3028(i2c = i2c)
 
 print(rtc.timestamp()) 
